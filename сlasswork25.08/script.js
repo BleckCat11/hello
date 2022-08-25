2327d891c3c7d472a829200bdf03b2bd1b8320a1
unknown <stepangurgelt@gmail.com>
2022-08-25 21:41:30 +0300

    class work 25. 08

---
+++ сlasswork25.08/script.js
@@ -0,0 +1,27 @@
+//   function numbers() {
+//   let input =  + document.getElementsByTagName("input")[0].value;
+//   let input2 = document.getElementById("input2");
+//   let mathTrunk = Math.trunc(input / 10)
+//   let operation1 = input - mathTrunk * 10
+//   input2.value = mathTrunk + "                " + operation1
+// }
+
+function counting() {
+    
+    let counter = 0;
+    let input = document.getElementById("input").value
+    alert (input)
+  while (input > 0) {
+    
+    if (input < 10) {
+      counter++;
+      input --
+      } else{
+        counter = counter + 2
+        input --
+      }
+   
+  }
+  alert (counter)
+  document.getElementById("input2").value = counter
+}
