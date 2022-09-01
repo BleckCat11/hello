081e1abcfbdb9a1c03a1e55bc74a1736de56498c
unknown <stepangurgelt@gmail.com>
2022-09-01 20:27:40 +0300

    home work

---
+++ homework01.09/script.js
@@ -0,0 +1,62 @@
+function f4 (){
+  alert ("dhfhadskf")
+  let d = []
+  while (i < 3){
+    alert ("fhjfjskdjhfjkadsf")
+  let a = prompt()
+  a.push(d)
+}
+  alert (d.join("-"))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function f3() {
+//   let y = prompt();
+//   let array = ["red", "yellow", "green"];
+//   for (let i = 0; i < array.length; i++) {
+//     if (y = array[i]) {
+//       document.body.style.backgroundColor = array[i];
+//     } else {
+//       alert("такого кольору немає");
+//       document.body.style.backgroundColor = "grey";
+//     }
+//   }
+// }
+
+// function f3() {
+//   let max1 = 100;
+//   let random = Math.floor(Math.random() * max1);
+//   document.getElementById("div").style.fontSize = random + "px"
+// }
+
+// function f2() {
+//   let input1 = +document.getElementById("input1");
+//   let input2 = document.getElementById("input2");
+//   input2.style.height = input1 + "px"
+//   input2.style.width = input2 + "px"
+// }
+
+// function f1 (){
+
+//     let input1 = + document.getElementById("input1").value
+//     let input2 = + document.getElementById("input2").value
+//     let input3 = + document.getElementById("input3").value
+//     alert (input3, input2, input1)
+//     document.body.style.backgroundColor = 'rgb(' + input1+',' + input2 + ',' + input3 + ')'
+
+// }
