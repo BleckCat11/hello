88db8f2b15a6ff20ddd230c67b628cb676016b2d
unknown <antonafonin9@gmail.com>
2022-09-12 21:35:44 +0300

    add comments

--- homework 12.09/script.js
+++ homework 12.09/script.js
@@ -3,8 +3,12 @@ function function1() {
   let divs = document.getElementsByTagName("div");
   for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener(
+      //Правильно mouseover - використовуй гугл, а також використовуй різні перевірки, щоб зрозуміти, чому не працює, код - наприклад,
+      //постав замість mauseover "click" - і перевір, чи проблема у цьому рядочку
       "mauseover",
       function () {
+        //потрібно використовувати e.target , а не divs[i]
+        //також формат кольору має бути rgb(123, 234, 45) - після rgb не має бути відступу
         divs[i].style.backgroundColor = 'rgb ( '+ 123 +' ,' +234+ ',' +45+') '
       },
       false
@@ -12,8 +16,14 @@ function function1() {
   }
 }
 
-function function2_buttom (){
+//Про це завдання поговоримо разом, однак підхід такий -
+// на прикладі function2_top: тобі потрібно отримати, який на момент натисканні кнопки top у діва (const currentTop = document.getElementById("div4").style.top). 
+//Потім від цього значення відняти 10px і присвоїти в стилі діву.
 
+function function2_buttom (){
+//чи є такий стиль, як buttom? будь ласка, гугли, перевіряй (навіть сам VS Code не видає buttom, коли ставиш крапку після style)
+//правильно - bottom
+// також після "=" нема числа
 document.getElementById("div4").style.buttom =  + "px"
 
 }
@@ -29,6 +39,8 @@ function function2_left (){
     document.getElementById("div4").style.left = 10 + "px"
 }
 
+//підхід такий самий як в 2 задачі - спочатку отримати стиль діва, потім задати новий
+//Функція не викликається
 function function3(){
     let div = document.getElementById("div6")
     div.addEventListener("click"
@@ -37,6 +49,7 @@ function function3(){
         document.getElementById("div4").style.height = + 10 + "px"
     },false
     )
+    //перевіряй правопис, правильно - dblclick (https://learn.javascript.ru/introduction-browser-events)
     div.addEventListener("dobleclick",
     function (e){
         document.getElementById("div4").style.height = - 10 + "px"
@@ -45,6 +58,8 @@ function function3(){
     )
 
 }
+
+//нема false третім параметром. Функція не викликається
 function function4 (){
     
     document.body.addEventListener("click", function (){
