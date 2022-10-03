e2aa021b5ad980529cb4842fc4cc0fcc0d7a5244
unknown <stepangurgelt@gmail.com>
2022-10-03 21:50:14 +0300

    classwork

---
+++ classwork 3.10/script.js
@@ -0,0 +1,32 @@
+let arrayOfColors = [];
+function correctNumber() {
+  let allInputs = document.getElementsByName("color");
+  for (let i of allInputs) {
+    if (i.value > 255 || i.value < 0) {
+      i.value = 0;
+    }
+  }
+}
+function save() {
+  let allInputs = document.getElementsByName("color");
+  let r = allInputs[0].value
+  let g = allInputs[1].value
+  let b = allInputs[2].value
+  createColor(r, g ,b);
+  console.log(arrayOfColors)
+}
+function createColor(a, b, c) {
+  let color = {
+    r: a,
+    g: b,
+    b: c,
+  };
+  arrayOfColors.push(color);
+}
+function changeColor(){
+let lastinput = + document.getElementById("lastinput").value
+let color = arrayOfColors[lastinput - 1]
+let result = 'rgb('+color.r+','+color.g+','+color.b+')'
+console.log(result)
+document.body.style.backgroundColor = result
+}
