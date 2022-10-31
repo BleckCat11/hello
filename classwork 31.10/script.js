e59f5da41510c3a337faa983e104f47870baeebc
unknown <stepangurgelt@gmail.com>
2022-10-31 20:34:03 +0200

    homework

--- classwork 31.10/script.js
+++ classwork 31.10/script.js
@@ -4,7 +4,7 @@ let number = document.getElementById("enteredNumber").value.length
 
 document
   .getElementById("enteredNumber")
-  .addEventListener("keydown", callFunctions(), false);
+  .addEventListener("keydown", callFunctions, false);
 
 //Фкнція виклику усіх функцій після зміни числа
 function callFunctions() {
@@ -28,33 +28,82 @@ function updateNumber() {
 
 // Функція для виведення результату у дів. Приймає в себе ід.
 function enterResult(id, result) {
-  id.value = result;
-  console.log ("result")
+  document.getElementById(id).value = +result;
 }
 
 function sumOfAllNumbers() {
-    alert ("fhafkjdhf")
-  let id = document.getElementById("task1");
+  let id = "task1";
   let stringNumber = String(number);
   for (let i of stringNumber) {
-console.log (i)
     let result = 0;
-    result = result + i
-    enterResult(id, result)
+    result = result + i;
+    enterResult(id, result);
   }
 }
 
 function sortAsc() {
+  let id = "task3";
+  let arr = [];
+  let stringNumber = String(number);
+  for (let i of stringNumber) {
+    arr.push(i);
+  }
+  let arr2 = arr.sort((a, b) => b + a);
+
+  enterResult(id, arr2);
 }
 
-function sortDesc() {}
+function sortDesc() {
+  let id = "task3";
+  let arr = [];
+  let stringNumber = String(number);
+  for (let i of stringNumber) {
+    arr.push(i);
+  }
+  let arr2 = arr.sort((a, b) => b - a);
 
-function maxNumber() {}
+  enterResult(id, arr2);
+}
 
-function maxMultiplicationOfThreeNumbers() {}
+function maxNumber() {
+  let stringNumber = String(number);
+  let max = 0;
+  for (let i of stringNumber) {
+    if (i > max) {
+      max == max - max + i;
+    }
+  }
+  enterResult(id, max);
+}
+
+function maxMultiplicationOfThreeNumbers() {
+  let counter = 0;
+  let stringNumber = String(number);
+  let max = 0;
+  while (counter != 3) {
+    for (let i of stringNumber) {
+      if (i > max) {
+        max == max - max + i;
+        stringNumber.slice(i, i - 1);
+      }
+      counter++;
+    }
+  }
+  enterResult(id, max);
+}
 
 function sumOfNumbersAtOddPlaces() {}
 
 function sqrtRounded() {}
 
-function nearestSimpleNumber() {}
+function nearestSimpleNumber() {
+  let id = "task8";
+  let stringNumber = String(number);
+  for (let i of stringNumber) {
+  }
+  for (let j = 2; j < i; j++) {}
+  if (i % j == 0) {
+    enterResult(id, i);
+    return;
+  }
+}
